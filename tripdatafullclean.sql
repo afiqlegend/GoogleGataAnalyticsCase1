159f83e03f1ed64b32d16fc5b70e9a378cf84398
Amirul Afiq Rosli <126535917+afiqlegend@users.noreply.github.com>
2023-04-09 21:29:20 +0800

    SQL and Tableau
    https://public.tableau.com/views/trip_16808231372340/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link

---
+++ tripdatafullclean.sql
@@ -0,0 +1,66 @@
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM january_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM february_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM march_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM april_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM may_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM june_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM july_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM august_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM september_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM october_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM november_tripdata
+UNION ALL
+
+SELECT ride_id, rideable_type,CAST( started_at AS DATE) AS date, DATEPART(year, started_at) AS year, DATENAME(MONTH, started_at) AS month,
+  FORMAT(CAST(started_at AS DATE), 'ddd') AS started_day, DATEDIFF(SECOND, started_at, ended_at) AS ride_length,  member_casual
+FROM december_tripdata
+
+ORDER BY date
+
+Select top 10 DATEPART(month, started_at) as month,DATEPART(year, started_at) as year
+From december_tripdata
+
+select MONTH(started_at) AS month,DATENAME(MONTH, started_at) AS month
+from december_tripdata
